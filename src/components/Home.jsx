bb1c343463cdfe6e47dfc473a7781ae4e65fb975
ANAND YADAV <113358205+ANANDSYADAV@users.noreply.github.com>
2024-01-09 14:19:40 +0530

    Added Typewriter functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,27 @@ import { SiLeetcode } from "react-icons/si";
 import { SiCodingninjas } from "react-icons/si";
 import { SiGeeksforgeeks } from "react-icons/si";
 import { Link } from 'react-scroll'
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 function Home() {
+
+  const [ text ] = useTypewriter({
+    words: ['ANAND YADAV', 'a Web Developer', 'a Problem Solver', 'a Coding Enthusiast', 'a NCC Cadet', 'a Learner'],
+    loop: true,
+    typeSpeed: 120,
+    deleteSpeed: 80,
+  });
 
   return (
     <div className="top-[80px] relative">
       <div id="home" className='flex flex-col bg-blue-100 justify-center items-center w-full min-h-[500px] gap-10 px-10 text-center'>
         <div className='flex flex-col gap-5 justify-center items-center'>
-          <p className='text-black font-bold text-5xl font-sans'>HEY, I'M ANAND YADAV</p>
+          <p className='text-black font-bold text-5xl font-sans'>
+            HEY, I'M <span className="text-red-900">{text}</span> 
+            <span className='text-green-900 font-semibold'>
+            <Cursor cursorStyle='|'/>
+            </span>
+          </p>
           <div className='flex flex-col gap-2 items-center'>
             <p className='text-black font-semibold text-md font-sans'>I am an undergraduate student at the National Institute of Technology, Agartala, studying computer science.</p>
             <p className='text-black font-semibold text-md font-sans'>I have a strong interest in web development and related fields.</p>
