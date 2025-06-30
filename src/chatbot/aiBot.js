25ef0396372e2b373dd781a1f5092473048dbbea
anandsyadav <ANANDSYADAV@users.noreply.github.com>
2025-06-30 15:14:16 +0530

    refactor aiBot.js

--- src/chatbot/aiBot.js
+++ src/chatbot/aiBot.js
@@ -18,9 +18,7 @@ const chatWithBot = async (userQuery) => {
             ]
         }
 
-        // let modifiedBodyContent = bodyContent.contents[0].parts[0].text + userQuery;
         const apiRes = await fetch(`${import.meta.env.VITE_GEMINI_API}?key=${import.meta.env.VITE_GEMINI_KEY}`, {
-            // const apiRes = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDjOzwnm47nV_WY7hMMkJYnfgWFJsVar0A`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -31,9 +29,6 @@ const chatWithBot = async (userQuery) => {
         const botRes = botResObj.candidates[0].content.parts[0].text;
         const cleanBotReplyArr = botRes.split('*').join(' ');
         console.log('CleanBotReply', cleanBotReplyArr);
-        // const [first, ...restAll] = cleanBotReplyArr;
-        // const markdown = `${first}\n${restAll.join(' ')}`;
-        // console.log('msg type:', typeof markdown, 'value:', cleanBotReplyArr);
         return cleanBotReplyArr;
     } catch (err) {
         return `Error: ${err.message}`;
